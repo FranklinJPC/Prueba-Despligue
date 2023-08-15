78b91a463c7635113443cc08cb8c32de1195e4c6
shiao-li <roberto.shiao@epn.edu.ec>
2023-08-14 23:04:30 -0500

    Test

--- src/components/Listar.jsx
+++ src/components/Listar.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 import Swal from 'sweetalert2'
 
 
-const Listar = ({ estado,setIdmetro }) => {
+const Listar = ({ estado, setIdmetro }) => {
     const [rutas, setRutas] = useState([])
 
     useEffect(() => {
@@ -69,10 +69,9 @@ const Listar = ({ estado,setIdmetro }) => {
                     :
                     rutas.map((ruta) => (
                         <div key={ruta.id} className="p-2 rounded-xl sm:flex gap-12 bg-gray-200 shadow-xl mb-5">
-
                             <img src={logoMetro} alt="art cover" className="sm:w-3/12 object-cover rounded-lg" />
 
-                            <div className="h-auto p-3 w-full">
+                            <div className='h-auto p-3 w-full flex flex-col'>
                                 <h4 className="text-2xl font-semibold text-cyan-900">{ruta.nombre}</h4>
                                 <hr className="w-full border border-gray-300 my-2" />
                                 <p className="text-gray-500">Sector: {ruta.sector}</p>
@@ -81,11 +80,12 @@ const Listar = ({ estado,setIdmetro }) => {
                                 <p className="text-gray-500">Maquinista: {ruta.maquinista}</p>
                                 <p className="text-gray-500">Detalles: {ruta.detalles}</p>
                                 <div className='flex justify-between mt-3 lg:justify-end md:justify-end gap-3'>
-                                    <button className='bg-sky-900 text-white px-6 py-1 rounded-full' onClick={()=>{setIdmetro(ruta.id)}}>Actualizar</button>
+                                    <button className='bg-sky-900 text-white px-6 py-1 rounded-full' onClick={() => { setIdmetro(ruta.id) }}>Actualizar</button>
                                     <button className='bg-red-900 text-white px-6 py-1 rounded-full' onClick={() => { handleDelete(ruta.id) }}>Eliminar</button>
                                 </div>
                             </div>
                         </div>
+
                     ))
             }
         </>
